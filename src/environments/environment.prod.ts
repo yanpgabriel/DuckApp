1e8b4440777412574105ba4812b3c4472dce4d66
Yan P. Gabriel <yanpgabriel@gmail.com>
2023-03-29 20:58:24 -0300

    ~ Trocando host da api em prod

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,6 +1,6 @@
 export const environment = {
   production: true,
-  api_host: 'http://192.168.0.3:8080/api',
+  api_host: 'https://duck.yanpgabriel.com/api',
   discord_bot_host: 'http://localhost:8069',
   ws_host: 'ws://localhost:8080',
   configFile: 'assets/config/config.prod.json',
