558722be63392ae28abe5026c4a0a57a4f46dbd7
Yan P. Gabriel <yanpgabriel@gmail.com>
2023-08-29 00:27:33 -0300

    ~ Diversas correções e melhorias

--- src/app/shared/services/tema.service.ts
+++ src/app/shared/services/tema.service.ts
@@ -1,6 +1,9 @@
 import { Inject, Injectable } from '@angular/core';
 import { DOCUMENT } from "@angular/common";
 
+const LIGHT_THEME = 'light'
+const DARK_THEME = 'dark'
+
 @Injectable({
   providedIn: 'root'
 })
@@ -17,14 +20,24 @@ export class TemaService {
   }
 
   toggleTheme() {
-    this.switchTheme(`${this.getCurrentTheme().includes('dark') ? 'light' : 'dark'}`);
+    this.switchTheme(`${this.getCurrentTheme().includes(DARK_THEME) ? LIGHT_THEME : DARK_THEME}`);
   }
 
   switchTheme(theme: string) {
     let themeLink = this.document.getElementById('app-theme') as HTMLLinkElement;
     if (themeLink) {
       themeLink.href = theme + '.css';
+      this.applyColor(theme);
       localStorage.setItem('theme', theme);
+    }
+  }
+
+  private applyColor(theme: string) {
+    let meta = this.document.getElementById('theme-color') as HTMLMetaElement;
+    if (theme === LIGHT_THEME) {
+      meta.content = '#ffffff';
+    } else {
+      meta.content = '#000000'
     }
   }
 }
